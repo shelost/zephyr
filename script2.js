5c48a9bc69277081f74e9030cc7360fd65fdcad7
Heewon Ahn <shelost.off@gmail.com>
2024-06-11 15:11:46 -0700

    resizing canvas

--- script2.js
+++ script2.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     let gridstackCounter = 4;
-    const gridstackContainer = document.getElementById('main');
+    const gridstackContainer = document.getElementById('screen');
     const gridstackConfigurations = {
         gridstack0: [
             { x: 0, y: 0, w: 2, h: 1, content: `assets/stan-mini.svg`, type: 'img' },
@@ -18,12 +18,10 @@ document.addEventListener('DOMContentLoaded', function() {
         grid: [
             {x: 0, y: 0, w: 4, h: 4, content: 'https://images.unsplash.com/photo-1490604001847-b712b0c2f967?q=80&w=2753&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', type: 'img'},
             {x: 4, y: 0, w: 4, h: 4, content: 'https://images.unsplash.com/photo-1472214103451-9374bd1c798e?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', type: 'img'},
-
             {x: 0, y: 4, w: 4, h: 4, content: 'https://images.unsplash.com/photo-1536152470836-b943b246224c?q=80&w=2838&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', type: 'img'},
             {x: 4, y: 4, w: 4, h: 4, content: 'https://images.unsplash.com/photo-1513875528452-39400945934d?q=80&w=2048&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', type: 'img'},
             {x: 0, y: 8, w: 4, h: 4, content: 'https://images.unsplash.com/photo-1500964757637-c85e8a162699?q=80&w=3003&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', type: 'img'},
-            {x: 4, y: 8, w: 4, h: 4, content: 'https://images.unsplash.com/photo-1559666126-84f389727b9a?q=80&w=2954&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', type: 'img'},
-
+            {x: 4, y: 8, w: 4, h: 4, content: 'https://images.unsplash.com/photo-1559666126-84f389727b9a?q=80&w=2954&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', type: 'img'}
         ],
         gridstack2: [
             {x: 0, y: 0, w: 6, h: 3, content: 'https://images.unsplash.com/photo-1559827291-72ee739d0d9a?q=80&w=2874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', type: 'img'},
@@ -31,7 +29,9 @@ document.addEventListener('DOMContentLoaded', function() {
         ]
     };
 
-
+    const freeformConfigurations = {
+        bro: []
+    };
 
     // Function to initialize a Gridstack instance
     function initializeGridstack(id, serializedData) {
@@ -68,6 +68,57 @@ document.addEventListener('DOMContentLoaded', function() {
                 setupTextareaEvents(widget, grid);
             }
         });
+    }
+
+    // Function to initialize a Freeform instance
+    function initializeFreeform(id) {
+        const container = document.createElement('div');
+        container.id = 'container' + id;
+        container.className = 'container';
+        container.innerHTML = `<canvas id="${id}"></canvas>`;
+        gridstackContainer.appendChild(container);
+
+        const canvas = new fabric.Canvas(id);
+        canvas.setWidth(400);
+        canvas.setHeight(container.offsetHeight);
+
+        // Add a rectangle to demonstrate Fabric.js
+        const rect = new fabric.Rect({
+            left: 0,
+            top: 250,
+            fill: '#ffce00',
+            width: 150,
+            height: 150,
+            rx: 10,
+            ry: 10,
+            angle: -15
+        });
+
+        const circle = new fabric.Circle({
+            left: 200,
+            top: 250,
+            radius: 100,
+            fill: 'red'
+        })
+
+        fabric.Image.fromURL('assets/jayhoovy.jpeg', function(myImg) {
+            var img1 = myImg.set({ left: 80, top: 40, width: 800, height: 800, scaleX: 0.3, scaleY: 0.3, angle: 5 });
+            canvas.add(img1);
+            canvas.sendToBack(img1)
+         });
+
+        rect.on('scaling', function() {
+            this.set({
+              width: this.width * this.scaleX,
+              height: this.height * this.scaleY,
+              scaleX: 1,
+              scaleY: 1
+            })
+        })
+
+
+        canvas.add(circle)
+        canvas.add(rect)
     }
 
     // Function to set up event listeners for textarea
@@ -116,8 +167,8 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 
-    // Function to render all Gridstacks
-    function renderGridstacks() {
+    // Function to render all Gridstacks and Freeform instances
+    function renderPages() {
         Object.keys(gridstackConfigurations).forEach(id => {
             if (!document.getElementById(id)) {
                 const gridElement = document.createElement('div');
@@ -131,71 +182,76 @@ document.addEventListener('DOMContentLoaded', function() {
                 initializeGridstack(id, gridstackConfigurations[id]);
             }
         });
+
+        Object.keys(freeformConfigurations).forEach(id => {
+            if (!document.getElementById(id)) {
+                const container = document.createElement('div');
+                container.id = 'container' + id;
+                container.className = 'container';
+                container.innerHTML = `<canvas id="${id}"></canvas>`;
+                //gridstackContainer.appendChild(container);
+                initializeFreeform(id);
+            }
+        });
     }
 
     // Initial render
-    renderGridstacks();
+    renderPages();
 
     // Add new Gridstack on button click
+    for (let i = 0; i < document.getElementsByClassName('add-page').length; i++) {
+        let button = document.getElementsByClassName('add-page')[i];
 
-    for (let i = 0; i < document.getElementsByClassName('add-page').length; i++){
-        let button = document.getElementsByClassName('add-page')[i]
-
-        button.addEventListener('click', function() {
+        button.addEventListener('click', function () {
+            console.log('to')
             gridstackCounter++;
             const newGridstackId = `gridstack${gridstackCounter}`;
 
-            let config = []
-
-
+            let config = [];
 
             switch (button.id) {
                 case 'add-1':
                     config = [
                         {x: 0, y: 0, w: 4, h: 4, content: 'https://images.unsplash.com/photo-1490604001847-b712b0c2f967?q=80&w=2753&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', type: 'img'},
                         {x: 4, y: 0, w: 4, h: 4, content: 'https://images.unsplash.com/photo-1490604001847-b712b0c2f967?q=80&w=2753&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', type: 'img'},
-
                         {x: 0, y: 4, w: 4, h: 4, content: 'https://images.unsplash.com/photo-1490604001847-b712b0c2f967?q=80&w=2753&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', type: 'img'},
                         {x: 4, y: 4, w: 4, h: 4, content: 'https://images.unsplash.com/photo-1490604001847-b712b0c2f967?q=80&w=2753&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', type: 'img'},
                         {x: 0, y: 8, w: 4, h: 4, content: 'https://images.unsplash.com/photo-1490604001847-b712b0c2f967?q=80&w=2753&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', type: 'img'},
-                        {x: 4, y: 8, w: 4, h: 4, content: 'https://images.unsplash.com/photo-1490604001847-b712b0c2f967?q=80&w=2753&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', type: 'img'},
-                    ]
-                    break
+                        {x: 4, y: 8, w: 4, h: 4, content: 'https://images.unsplash.com/photo-1490604001847-b712b0c2f967?q=80&w=2753&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', type: 'img'}
+                    ];
+                    break;
                 default:
-                    break
+                    break;
             }
 
-            gridstackConfigurations[newGridstackId] = config
-            let main =  document.getElementById('main')
+            gridstackConfigurations[newGridstackId] = config;
+            let main = document.getElementById('screen');
 
-            renderGridstacks();
-            main.scrollTo({top: main.scrollHeight, left: 0, behavior: 'smooth'})
+            renderPages();
+            main.scrollTo({top: main.scrollHeight, left: 0, behavior: 'smooth'});
         });
     }
 
+    for (let i = 0; i < document.getElementsByClassName('grid-stack').length; i++) {
+        let page = document.getElementsByClassName('grid-stack')[i];
 
-    for (let i = 0; i < document.getElementsByClassName('grid-stack').length; i++){
-        let page = document.getElementsByClassName('grid-stack')[i]
-
-
-            page.addEventListener('mouseenter', e => {
-                page.classList.add('hover')
-            })
-            page.addEventListener('mouseleave', e => {
-                page.classList.remove('hover')
-            })
-
+        page.addEventListener('mouseenter', e => {
+            page.classList.add('hover');
+        });
+        page.addEventListener('mouseleave', e => {
+            page.classList.remove('hover');
+        });
     }
+
+    // Add Freeform page on button click
+    document.getElementById('add-freeform').addEventListener('click', function() {
+        gridstackCounter++;
+        const newFreeformId = `freeform${gridstackCounter}`;
+        freeformConfigurations[newFreeformId] = []
+        renderPages();
+        let main = document.getElementById('screen');
+        main.scrollTo({top: main.scrollHeight, left: 0, behavior: 'smooth'});
+    });
 
     console.log('Gridstack initialized and data loaded');
 });
-
-
-
-
-
-
-
-
-
-
